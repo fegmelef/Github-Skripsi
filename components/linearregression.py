7dae44c189a47802a5ce65fa6e13c2d59d40f71f
fegmelef <icepanda38@gmail.com>
2025-06-30 10:44:19 +0700

    Update linearregression.py

--- components/linearregression.py
+++ components/linearregression.py
@@ -11,7 +11,7 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
-import statsmodels.api as sm
+# import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
